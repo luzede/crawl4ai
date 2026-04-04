162b530f78acd27b155b004f3331295c4e671085
Loukas Zervakis <85195923+luzede@users.noreply.github.com>
2026-04-05 02:54:04 +0400

    Add RunReturn type for arun() function
    arun() has the same return type as arun_many() but arun_many() is an async generator because it yields, but arun() does not yield

--- crawl4ai/models.py
+++ crawl4ai/models.py
@@ -322,6 +322,12 @@ RunManyReturn = Union[
     AsyncGenerator[CrawlResultT, None]
 ]
 
+RunReturn = Union[
+    CrawlResultContainer[CrawlResultT],
+    CrawlResultT
+]
+    
+
 
 # END of backward compatibility code for markdown/markdown_v2.
 # When removing this code in the future, make sure to:
